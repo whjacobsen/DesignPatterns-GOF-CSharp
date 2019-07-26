0230e688f8c214ab548cb4d27c3fd2e961ea6ca7
William Holm Jacobsen <whjacobsen@gmail.com>
2019-07-27 01:00:25 +0200

    Structure of behavioral patterns and tests done

---
+++ src/Behavioral Patterns/Tests/Test.Chain of Responsibility/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Test.Chain_of_Responsibility
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
