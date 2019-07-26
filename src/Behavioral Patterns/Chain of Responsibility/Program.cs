031c2a71887b1aaa86914d75c88867434a5dd845
William Holm Jacobsen <whjacobsen@gmail.com>
2019-07-26 10:22:15 +0200

    Folder and project structure inplace

---
+++ src/Behavioral Patterns/Chain of Responsibility/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Chain_of_Responsibility
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/Creational Patterns/Abstract Factory/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Abstract_Factory
+{
+    /// <summary>
+    /// Lets you produce families of related objects without specifying their concrete classes.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
