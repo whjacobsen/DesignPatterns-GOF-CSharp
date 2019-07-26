1e96fe19a596a91ba6f5df104e37b80176a729d0
William Holm Jacobsen <whjacobsen@gmail.com>
2019-07-26 14:50:54 +0200

    Builder pattern and test project added

---
+++ src/Creational Patterns/Builder/Builder/Interfaces/IBuilder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Builder.Interfaces
+{
+    // The Builder interface specifies methods for creating the different parts
+    // of the Product objects.
+    public interface IBuilder
+    {
+        void BuildPartA();
+
+        void BuildPartB();
+
+        void BuildPartC();
+    }
+}
---
+++ src/Creational Patterns/Builder/Builder/Program.cs
@@ -0,0 +1,35 @@
+﻿using Builder.Implementations;
+using System;
+
+namespace Builder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // The client code creates a builder object, passes it to the
+            // director and then initiates the construction process. The end
+            // result is retrieved from the builder object.
+            var director = new Director();
+            var builder = new ConcreteBuilder();
+            director.Builder = builder;
+
+            Console.WriteLine("Standard basic product:");
+            director.buildMinimalViableProduct();
+            Console.WriteLine(builder.GetProduct().ListParts());
+
+            Console.WriteLine("Standard full featured product:");
+            director.buildFullFeaturedProduct();
+            Console.WriteLine(builder.GetProduct().ListParts());
+
+            // Remember, the Builder pattern can be used without a Director
+            // class.
+            Console.WriteLine("Custom product:");
+            builder.BuildPartA();
+            builder.BuildPartC();
+            Console.Write(builder.GetProduct().ListParts());
+
+            Console.ReadLine();
+        }
+    }
+}
